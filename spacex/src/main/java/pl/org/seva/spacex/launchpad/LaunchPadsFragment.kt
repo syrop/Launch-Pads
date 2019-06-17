6901bdc10adfac4e59b6456b8ab2d939b8d5d350
wiktor <wiktor.nizio@gmail.com>
2019-06-17 23:27:06 +0200

    Moved viewModel to a field.

--- spacex/src/main/java/pl/org/seva/spacex/launchpad/LaunchPadsFragment.kt
+++ spacex/src/main/java/pl/org/seva/spacex/launchpad/LaunchPadsFragment.kt
@@ -2,18 +2,20 @@ package pl.org.seva.spacex.launchpad
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.navGraphViewModels
 import kotlinx.coroutines.launch
 import pl.org.seva.spacex.R
 import pl.org.seva.spacex.main.extension.invoke
 
 class LaunchPadsFragment : Fragment(R.layout.fr_list) {
 
+    private val vm by navGraphViewModels<LaunchPadViewModel>(R.id.nav_graph)
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        (viewModels<LaunchPadViewModel>().value.ld to this) { response ->
+        (vm.ld to this) { response ->
             if (response is LaunchPadViewModel.Status.Success) {
                 for (launchPad in response.list) {
                     lifecycleScope.launch {
