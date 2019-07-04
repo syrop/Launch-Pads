1791435c4d73173789f9579e3e3803cb9fc2d527
wiktor <wiktor.nizio@gmail.com>
2019-07-04 16:29:16 +0200

    Removed a redundant function.

--- launchpads/src/main/java/pl/org/seva/launchpads/main/extension/LiveData.kt
+++ launchpads/src/main/java/pl/org/seva/launchpads/main/extension/LiveData.kt
@@ -21,11 +21,7 @@ package pl.org.seva.launchpads.main.extension
 
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.Observer
+import androidx.lifecycle.observe
 
 operator fun <T> LiveData<T>.invoke(owner: LifecycleOwner, observer: (T) -> Unit) =
         observe(owner) { observer(it) }
-
-@Suppress("unused")
-fun <T> LiveData<T>.observe(owner: LifecycleOwner, observer: (T) -> Unit) =
-        observe(owner, Observer<T> { observer(it) })
